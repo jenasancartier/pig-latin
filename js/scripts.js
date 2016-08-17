0823958d872c40b0307fd495d7d023c92277cc8a
Jena SanCartier and Kevin Johnston <kevin@>
2016-08-17 13:28:21 -0700

    add second spec: adds ay to single-letter words beginning with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-(document).ready();
+//back-end logic
+$(document).ready(function() {
+	$("form#pig-latin").submit(function(event){
+		event.preventDefault();
+
+var vowels = ["a", "e", "i", "o", "u"];
+var result = [];
+console.log($("#englishInput").val());
+var splitArray = $("#englishInput").val().toLowerCase().split("");
+	for (var i = 0; i < splitArray.length; ++i){
+		if(vowels.includes(splitArray[0])) {
+			result.push(splitArray[i] + "ay");
+		} else {
+		result.push(splitArray[i]);
+		}
+	}
+	console.log(result)
+
+
+
+
+
+//front-end logic
+
+	});
+});
