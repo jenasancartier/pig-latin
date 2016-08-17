7098aaf51c6ff7e665b7ebfa6d5edbcd1e5e5e54
Jena SanCartier and Kevin Johnston <kevin@>
2016-08-17 15:05:55 -0700

    add fourth spec: move the first consonant to the end of a word beginning with one consonant

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,16 @@ $(document).ready(function() {
 		event.preventDefault();
 
 var vowels = ["a", "e", "i", "o", "u"];
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+console.log(consonants);
 var result = [];
 console.log($("#englishInput").val());
 var splitArray = $("#englishInput").val().toLowerCase().split("");
 	for (var i = 0; i < 1; ++i){
 		if(vowels.includes(splitArray[0])) {
 			result.push("a", "y");
+		} else if (consonants.includes(splitArray[0])){
+			result.push(splitArray.shift())
 		} else {
 		result.push(splitArray[i]);
 		}
